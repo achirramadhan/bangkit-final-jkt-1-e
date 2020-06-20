8666f92ab92a2709ed501fb928b7b5cd1953970f
Muhamad Achir Suci Ramadhan <muhamadachir99@gmail.com>
2020-06-20 17:31:29 +0700

    Add route for prediction

--- Backend/app.py
+++ Backend/app.py
@@ -1,20 +1,41 @@
-from flask import Flask, request
+import numpy as np
+import io
+import tensorflow as tf
+
+from flask import Flask, request, send_file
 from inference import Inference
+
 
 # initiat the inference, doing this outside any
 # function as it only run once
-inference = Inference('./cnn_do.h5')
+inference = Inference("./cnn_do.h5")
 
 app = Flask(__name__)
 
-@app.route('/')
+
+@app.route("/")
 def index():
-  return {"root": True}
+    return {"root": True}
+
 
 # call this function for prediction or
 # simply call the function inside this function
 def predict(image):
-  return inference.predict(image)
+    return inference.predict(image)
+
+
+@app.route("/predict", methods=["POST"])
+def get_prediction():
+    image = request.files["image"]
+
+    in_memory_file = io.BytesIO()
+    image.save(in_memory_file)
+
+    tf_image = tf.io.decode_image(in_memory_file.getvalue())
+    result = predict(tf_image)
+    return {"result": result}
+
 
 if __name__ == "__main__":
-  app.run(host='0.0.0.0', debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
+
--- Backend/inference.py
+++ Backend/inference.py
@@ -1,16 +1,19 @@
+import numpy as np
+import json
 import tensorflow as tf
+
 from tensorflow.keras.models import load_model
 from tensorflow.image import resize
 from tensorflow.image import rgb_to_grayscale
 from tensorflow.keras.preprocessing.image import img_to_array
-import numpy as np
 
-class Inference():
-    
+
+class Inference:
+
     # The labels in the exact order when training,
     # Please do not change this.
-    labels = ['angry', 'disgust', 'fear', 'happy', 'neutral', 'sad', 'surprise']
-    
+    labels = ["angry", "disgust", "fear", "happy", "neutral", "sad", "surprise"]
+
     def __init__(self, modelpath):
         try:
             self.model = load_model(modelpath)
@@ -20,24 +23,28 @@ class Inference():
 
     def predict(self, image):
         image = self.preprocess(image)
-        pred = self.model.predict(image)
-        pred = np.argmax(pred)
-        return self.labels[pred]
+        preds = self.model.predict(image)
+        pred = np.argmax(preds)
+        return {
+            "label": self.labels[pred],
+            "probs": dict(zip(self.labels, preds.flatten().astype("float"))),
+        }
 
     def preprocess(self, image):
-        image = resize(image, (self.inp_shape[1], self.inp_shape[2])) #rescale to match the input size
-        
-        #change to grayscale if the model is in grayscale
-        if self.inp_shape[3] == 1:
+        image = resize(
+            image, (self.inp_shape[1], self.inp_shape[2])
+        )  # rescale to match the input size
+
+        # change to grayscale if the model is in grayscale
+        if self.inp_shape[3] == 1 and image.shape[2] != 1:
             image = rgb_to_grayscale(image)
 
         image = img_to_array(image)
         image = np.expand_dims(image, axis=0)
         return image
-    
 
 
-if __name__ == '__main__':
-    dummy_image = np..random.random((50,50,3))
-    inference = Inference('./cnn_do.h5')
+if __name__ == "__main__":
+    dummy_image = np.random.random((50, 50, 3))
+    inference = Inference("./cnn_do.h5")
     print(inference.predict(dummy_image))
